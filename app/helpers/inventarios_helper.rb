def71944abf4f75253975b2ac079a85f5e8d5675
Juan Zubillaga <juan@zubi.net>
2010-08-01 02:44:45 +0200

    Se ven fotos pero no funciona edit ;

--- app/helpers/inventarios_helper.rb
+++ app/helpers/inventarios_helper.rb
@@ -1,2 +1,10 @@
 module InventariosHelper
+  def foto_for(inventario, size = :medium)
+    if inventario.foto 
+      foto_image = inventario.foto.public_filename(size) 
+      link_to image_tag(foto_image), inventario.foto.public_filename
+    else
+      image_tag("blank-foto-#{size}.png" )
+    end 
+  end
 end
